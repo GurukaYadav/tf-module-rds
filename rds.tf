2480d852eb2c7d18aff3bf7486efdd59853f9a9a
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-27 20:02:29 +0530

    updating tf-module-rds

--- rds.tf
+++ rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "rds" {
   parameter_group_name = var.PARAMETER_GROUP_NAME
   skip_final_snapshot  = true
   db_subnet_group_name = var.PRIVATE_SUBNET_ID
-  vpc_security_group_ids = [aws_security_group.rds.id]
+  vpc_security_group_ids = "[aws_security_group.rds.id]"
   identifier = "local.NAME_TAG-rds"
   PROJECT = var.PROJECT
   ENV = var.ENV
