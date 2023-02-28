8c4808335c6cc2afb495f83b2d286daede48b7c5
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-28 11:23:45 +0530

    updating tf-module-rds

--- rds.tf
+++ rds.tf
@@ -1,15 +1,15 @@
 resource "aws_db_instance" "rds" {
-  allocated_storage    = 10
-  db_name              = var.DB_NAME
-  engine               = var.ENGINE
-  engine_version       = var.ENGINE_VERSION
-  instance_class       = var.RDS_INSTANCE_CLASS
-  username             = var.USER_NAME
-  password             = var.PASSWORD
-  parameter_group_name = var.PARAMETER_GROUP_NAME
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.rds.name
+  allocated_storage      = 10
+  db_name                = var.DB_NAME
+  engine                 = var.ENGINE
+  engine_version         = var.ENGINE_VERSION
+  instance_class         = var.RDS_INSTANCE_CLASS
+  username               = var.USER_NAME
+  password               = var.PASSWORD
+  parameter_group_name   = var.PARAMETER_GROUP_NAME
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.rds.id]
-  identifier = "${local.TAG_NAME}-rds"
+  identifier             = "${local.TAG_NAME}-rds"
 
 }
