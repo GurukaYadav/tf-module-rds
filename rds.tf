71c6efdc5449ca6135166447f663b5e4ac3468d2
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-28 17:43:21 +0530

    updating tf-module-rds

--- rds.tf
+++ rds.tf
@@ -7,7 +7,6 @@ resource "aws_db_instance" "rds" {
   username               = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]
   password               = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]
   parameter_group_name   = aws_db_parameter_group.pg.name
-  skip_final_snapshot    = true
   db_subnet_group_name   = aws_db_subnet_group.rds.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   identifier             = "${local.TAG_NAME}-instance"
