046786dd677fb554891bbae79d6048e3eeb80eff
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-08 14:37:06 +0530

    updating tf-module-rds

---
+++ schema.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "null" {
+  provisioner "local-exec" {
+    command = <<EOF
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${RDS_ENDPOINT} -u${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]}  -p${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]} <shipping.sql
+EOF
+  }
+}
